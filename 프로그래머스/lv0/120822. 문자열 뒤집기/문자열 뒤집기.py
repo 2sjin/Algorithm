bbd7ff9df94f9d84f0ca6a22a029d40e613222d1
Seungjin Yi <options3224@gmail.com>
2023-01-30 23:17:50 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,7 +1,10 @@
+"""
 def solution(my_string):
     str_list = list(my_string)
     str_list.reverse()
     answer = "".join(str_list)
     return answer
+"""
 
-    # return my_string[::-1]
+def solution(my_string):
+    return my_string[::-1]
