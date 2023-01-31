bf3fc5b6a8aa609a585a1d9adbb9744733d51e19
Seungjin Yi <options3224@gmail.com>
2023-01-31 11:48:38 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -1,7 +1,18 @@
+"""
 def solution(numbers, direction):
     answer = numbers
     if direction == "right":
         answer.insert(0, answer.pop())
     else:
         answer.append(answer.pop(0))
+    return answer
+"""
+
+# 리스트 슬라이싱 활용
+def solution(numbers, direction):
+    answer = numbers
+    if direction == "right":
+        answer = answer[-1:] + answer[:-1]
+    else:
+        answer = answer[1:] + answer[:1]
     return answer
