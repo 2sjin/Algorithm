896b8cde379c0d60568c60469713c2d0ce44d5f3
Seungjin Yi <options3224@gmail.com>
2023-01-31 12:26:43 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,2 +1,9 @@
+"""
 def solution(my_string, num1, num2):
     return my_string[:num1] + my_string[num2] + my_string[num1+1:num2] + my_string[num1] + my_string[num2+1:]
+"""
+
+def solution(my_string, num1, num2):
+    answer = list(my_string)
+    answer[num1], answer[num2] = answer[num2], answer[num1]
+    return "".join(answer)
