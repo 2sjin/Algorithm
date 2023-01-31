0d5bf9a0bc0914e69490b95445b15aa64e0c194d
Seungjin Yi <options3224@gmail.com>
2023-01-31 13:30:26 +0900

    [level 0] Title: 가위 바위 보, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,5 +1,11 @@
+"""
 def solution(rsp):
-    answer = rsp
-    answer = answer.replace("0", "p").replace("2", "r").replace("5", "s")
+    answer = rsp.replace("0", "p").replace("2", "r").replace("5", "s")
     answer = answer.replace("r", "0").replace("s", "2").replace("p", "5")
     return answer
+"""
+
+# 딕셔너리 사용
+def solution(rsp):
+    dic = {"0":"5", "2":"0", "5":"2"}    
+    return "".join(dic[x] for x in rsp)
