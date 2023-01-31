145e580a6a7316eb644a4e7ae95139c01c0cef95
Seungjin Yi <options3224@gmail.com>
2023-01-31 22:21:53 +0900

    [level 2] Title: 올바른 괄호, Time: 6.60 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,11 @@
+def solution(s):
+
+    flag = 0
+    for ch in s:
+        # 여는 괄호면 +1, 닫는 괄호면 -1
+        flag += 1 if ch == "(" else -1
+        if flag < 0:         # 열기도 전에 닫으면 False
+            return False
+    
+    # 여는 괄호와 닫는 괄호 쌍이 맞아야 True
+    return True if flag == 0 else False
