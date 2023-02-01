7f63bd8aa1f8cf0e8dd88d343e3a1f18d48c51a9
Seungjin Yi <options3224@gmail.com>
2023-02-02 02:32:51 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -1,3 +1,4 @@
+"""
 from collections import Counter
 
 def solution(lines):
@@ -9,3 +10,9 @@ def solution(lines):
         
     dic = Counter(res)
     return len([i[0] for i in dic.items() if i[1] >= 2])
+"""
+
+# 집합 사용해서 풀기
+def solution(lines):
+    sets = [set(range(lines[i][0], lines[i][1])) for i in range(3)]
+    return len(sets[0] & sets[1] | sets[1] & sets[2] | sets[2] & sets[0])
