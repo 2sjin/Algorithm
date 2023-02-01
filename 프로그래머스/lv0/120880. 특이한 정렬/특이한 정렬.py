5b3c7b96141dd23b62b3e33b50ca686234bc5115
Seungjin Yi <options3224@gmail.com>
2023-02-02 01:21:43 +0900

    [level 0] Title: 특이한 정렬, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.py
@@ -0,0 +1,7 @@
+def solution(numlist, n):
+    dic = {}
+    order = [abs(n-i) for i in numlist]
+    for i in range(len(numlist)):
+        dic[numlist[i]] = order[i] 
+    res = sorted(dic.items(), key=lambda x: (x[1], x[0] * -1))
+    return [i[0] for i in res]
