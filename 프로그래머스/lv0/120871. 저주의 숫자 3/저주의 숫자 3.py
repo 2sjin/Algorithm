856ae0f76c1360cff02b53842567e4bb35577458
Seungjin Yi <options3224@gmail.com>
2023-02-02 02:05:57 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -1,3 +1,3 @@
 def solution(n):
-    numbers = [i for i in range(1, n*3) if (i % 3 != 0) and ("3" not in str(i))]
+    numbers = [i for i in range(186) if (i % 3 != 0) and ("3" not in str(i))]
     return numbers[n-1]
