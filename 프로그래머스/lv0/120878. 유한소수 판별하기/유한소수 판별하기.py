88fe736d80aa47e3434944d1a99affb7b459061d
Seungjin Yi <options3224@gmail.com>
2023-02-02 15:49:45 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -1,3 +1,4 @@
+"""
 from math import gcd
 
 def prime_tuple(n):
@@ -21,3 +22,15 @@ def solution(a, b):
                 break
     
     return 1 if sorted(list(set(res) | set([2, 5]))) == [2, 5] else 2
+"""
+
+# 소인수가 2와 5만 존재 -> 나머지가 없도록 2와 5로 계속 나누다보면, 1이 되어야 함
+from math import gcd
+def solution(a, b):
+    denom = b // gcd(a, b)
+    while denom % 2 == 0:
+        denom //= 2
+    while denom % 5 == 0:
+        denom //= 5
+    
+    return 1 if denom == 1 else 2
