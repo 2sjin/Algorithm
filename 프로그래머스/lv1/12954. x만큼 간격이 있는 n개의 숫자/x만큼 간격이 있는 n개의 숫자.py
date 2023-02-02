c83e20025e22a08748e4b2d449f1ed117d16c520
Seungjin Yi <options3224@gmail.com>
2023-02-02 22:57:00 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.11 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.py
+++ 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.py
@@ -1,2 +1,7 @@
+"""
 def solution(x, n):
     return [i for i in range(x, x*n+(x//abs(x)), x)] if x != 0 else [0 for i in range(n)]
+"""
+
+def solution(x, n):
+    return [x + (x*i) for i in range(n)]
