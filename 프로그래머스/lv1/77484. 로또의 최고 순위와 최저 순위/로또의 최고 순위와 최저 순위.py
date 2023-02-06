b54325c996bd22e17b85641ac8a96cc7e266dc63
Seungjin Yi <options3224@gmail.com>
2023-02-06 13:46:40 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,3 +1,4 @@
+"""
 def solution(lottos, win_nums):
     answer = [7, 7]
 
@@ -12,3 +13,14 @@ def solution(lottos, win_nums):
     answer[1] = 6 if answer[1] == 7 else answer[1]
     
     return answer
+"""
+
+def solution(lottos, win_nums):
+    rank = (6, 6, 5, 4, 3, 2, 1)    # 번호 index개 일치하면 순위 value등
+    
+    match_cnt = 0
+    for i in lottos:
+        if i in win_nums:
+            match_cnt += 1
+            
+    return rank[match_cnt + lottos.count(0)], rank[match_cnt]
