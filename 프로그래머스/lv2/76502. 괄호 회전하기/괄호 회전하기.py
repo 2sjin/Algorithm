86685e40dd8a1447bf30e6804a05b2b314d0bf60
Seungjin Yi <options3224@gmail.com>
2023-02-08 17:27:08 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,11 +1,13 @@
+from collections import deque
+
 def check_correct(sub_str):
-    stack = []
+    stack = deque()
     for ch in sub_str:
         if ch in "({[":
             stack.append(ch)
-        elif stack == [] or stack.pop() + ch not in ("()", "{}", "[]"):
+        elif len(stack) == 0 or stack.pop() + ch not in ("()", "{}", "[]"):
             return False
-    return True if stack == [] else False
+    return True if len(stack) == 0 else False
 
 def solution(s):
     result = 0
