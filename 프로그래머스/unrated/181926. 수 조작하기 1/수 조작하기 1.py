7a5a1769c0a145d3403b1c0ae3ea1b9c18e31f39
Seungjin Yi <options3224@gmail.com>
2023-05-13 13:21:51 +0900

    [unrated] Title: 수 조작하기 1, Time: 24.50 ms, Memory: 19.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,6 +1,11 @@
 def solution(n, control):
+    dic = {"w":"+1", "s":"-1", "d":"+10", "a":"-10"}
+    return n + eval("".join(dic[x] for x in control))
+    
+    """
     control = control.replace("w", "+1")
     control = control.replace("s", "-1")
     control = control.replace("d", "+10")
     control = control.replace("a", "-10")
     return eval(str(n) + control)
+    """
