719374cd51d655dd6fcd548c23d7314b103c0618
Seungjin Yi <options3224@gmail.com>
2023-11-08 22:46:26 +0900

    [level unrated] Title: 할 일 목록, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.py
@@ -1,6 +1,11 @@
 def solution(todo_list, finished):
+    return [todo_list[i] for i in range(len(todo_list)) if not finished[i]]
+
+"""
+def solution(todo_list, finished):
     res = []
     for i in range(len(todo_list)):
         if not finished[i]:
-            res.append(todo_list[i])
+            res.append(todo_list[i])            
     return res
+"""
