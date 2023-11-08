dac7fc3ffe10caa97d7cf1ff5127d08f2961a741
Seungjin Yi <options3224@gmail.com>
2023-11-08 22:27:35 +0900

    [level unrated] Title: n개 간격의 원소들, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.py
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.py
@@ -1,5 +1,11 @@
 def solution(num_list, n):
+    return num_list[::n]
+
+
+"""
+def solution(num_list, n):
     res = []
     for i in range(0, len(num_list), n):
         res.append(num_list[i])
     return res
+"""
