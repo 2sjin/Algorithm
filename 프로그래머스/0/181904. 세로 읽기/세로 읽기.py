06aa8a5a3dcba4294fb9d13e9671621b6e828e02
Seungjin Yi <options3224@gmail.com>
2024-01-26 20:59:06 +0900

    [level 0] Title: 세로 읽기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -0,0 +1,8 @@
+def solution(my_string, m, c):
+    result = ""
+    cursor = 0
+    for i in range(len(my_string)//m):
+        s = my_string[cursor:cursor+m]
+        result += s[c-1]
+        cursor += m
+    return result
