382d406a5fb14b5644d16a5bf97f25d793d48fbb
Seungjin Yi <options3224@gmail.com>
2024-06-26 23:30:10 +0900

    [level 0] Title: 배열 비교하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.py
@@ -0,0 +1,13 @@
+def solution(arr1, arr2):
+    len1, len2 = len(arr1), len(arr2)
+    if len1 > len2:
+        return 1
+    elif len1 < len2:
+        return -1
+    else:
+        sum1, sum2 = sum(arr1), sum(arr2)
+        if sum1 > sum2:
+            return 1
+        elif sum1 < sum2:
+            return -1
+    return 0
