e7202c6d2b706907f8ec9135ead24fc9bdb0e8b3
Seungjin Yi <options3224@gmail.com>
2024-10-26 21:52:36 +0900

    [level 0] Title: 문자열 출력하기, Time: 63.93 ms, Memory: 31.8 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.js
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.js
@@ -1,8 +1,5 @@
 const fs = require('fs')
-
-const input = fs.readFileSync('/dev/stdin').toString()
-
+const input = fs.readFileSync('/dev/stdin').toString().trim()
 
 let str = input
-
 console.log(str)
